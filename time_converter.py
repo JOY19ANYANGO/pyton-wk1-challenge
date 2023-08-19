bd20c7719271fb2c58648d4493bdb1159b6149a6
JOY19ANYANGO <joyanyango402@gmail.com>
2023-08-20 01:02:32 +0300

    made few corrections

--- time_converter.py
+++ time_converter.py
@@ -1,12 +1,12 @@
 def time_converter(hour ,minute,period):
-    if (hour>=0 and hour<12) and period=="pm" and(minute>=0 and minute<=59):
+    if (hour>0 and hour<12) and period=="pm" and(minute>=0 and minute<=59):
          hour += 12
          return f"{hour:02d}:{minute:02d}"
          
     elif hour==12 and (minute>=0 and minute<=59) and period=="pm":
         return f"{hour:02d}:{minute:02d}"
     
-    elif (hour>=0 and hour<12) and period=="am" and(minute>=0 and minute<=59):
+    elif (hour>0 and hour<12) and period=="am" and(minute>=0 and minute<=59):
          return f"{hour:02d}:{minute:02d}"
     
     elif hour==12 and (minute>=0 and minute<=59) and period=="am":
